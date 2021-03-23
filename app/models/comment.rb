c59eb064583195d89aab99f25ba4ed43e9256365
github username <github email address>
2021-03-23 16:08:21 -0500

    1 fail

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def users_attributes=(ua)
+    self.user = User.find_or_create_by(username: users_attributes[:username]) unless users_attributes[:username].blank?
+  end
 
 end
